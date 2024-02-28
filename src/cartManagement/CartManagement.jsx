e4d8b33ca87317c8a1ca0f68fc3b8c713f5cfbe1
atikur-7 <asraful0508@gmail.com>
2024-02-28 12:42:30 +0600

    create topbar and resposive for muiltiple devices

--- src/cartManagement/CartManagement.jsx
+++ src/cartManagement/CartManagement.jsx
@@ -1,9 +1,11 @@
+import TopMenu from "./TopMenu";
+
 const CartManagement = () => {
-    return (
-        <section className="w-2/5">
-            <p>Cart Management</p>
-        </section>
-    );
+  return (
+    <section className="mt-12 w-2/5">
+      <TopMenu />
+    </section>
+  );
 };
 
 export default CartManagement;
--- src/products/ProductCard.jsx
+++ src/products/ProductCard.jsx
@@ -4,7 +4,7 @@ const ProductCard = ({ product }) => {
   const { id, name, image, price, quantity } = product || {};
   return (
     <div className="pointer-events-auto relative flex w-full cursor-pointer flex-col rounded border border-gray-300 text-[13px] text-gray-500 transition-all active:opacity-70">
-      <div className="h-[150px] transition-all 2xl:h-[164px]">
+      <div className="h-[145px] transition-all 2xl:h-[164px]">
         <img className="h-full w-full object-contain" src={image} alt={name} />
       </div>
       {quantity === 0 && (
@@ -12,7 +12,7 @@ const ProductCard = ({ product }) => {
       )}
       <div className="py-1 text-center md:py-1.5">
         <p className="font-semibold">${price}</p>
-        <hr />
+        <hr className="py-0.5" />
         <h2>{name}</h2>
       </div>
     </div>
