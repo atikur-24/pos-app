3655f3332f22cfd3e476f2fc1e1502c3158fb0f1
atikur-7 <asraful0508@gmail.com>
2024-02-28 11:35:51 +0600

    render all products and responsive for multiple devices

---
+++ src/products/ProductCard.jsx
@@ -0,0 +1,22 @@
+import { PiWarningOctagonBold } from "react-icons/pi";
+
+const ProductCard = ({ product }) => {
+  const { id, name, image, price, quantity } = product || {};
+  return (
+    <div className="pointer-events-auto relative flex w-full cursor-pointer flex-col rounded border border-gray-300 text-[13px] text-gray-500 transition-all active:opacity-70">
+      <div className="h-[150px] transition-all 2xl:h-[164px]">
+        <img className="h-full w-full object-contain" src={image} alt={name} />
+      </div>
+      {quantity === 0 && (
+        <PiWarningOctagonBold className="absolute right-2 top-2 text-base text-red-600/90" />
+      )}
+      <div className="py-1 text-center md:py-1.5">
+        <p className="font-semibold">${price}</p>
+        <hr />
+        <h2>{name}</h2>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/products/Products.jsx
+++ src/products/Products.jsx
@@ -1,16 +1,21 @@
+import { useState } from "react";
+import productData from "../../public/products.json";
+import ProductCard from "./ProductCard";
 import Search from "./Search";
 
 const Products = () => {
-    return (
-        <section className="w-3/5 bg-[#f4f6f8] mt-10">
-            {/* search product */}
-            <Search />
-            <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit eos tenetur veniam vel aspernatur quam pariatur sed illo, architecto iste vitae, nobis, nisi animi ipsam fugit
-                laborum quibusdam reprehenderit nesciunt?
-            </p>
-        </section>
-    );
+  const [products, setProducts] = useState(productData);
+  return (
+    <section className="w-3/5 bg-[#f4f6f8]">
+      {/* search product */}
+      <Search />
+      <div className="grid grid-cols-2 gap-1 md:grid-cols-3 md:gap-2 lg:grid-cols-4 lg:gap-3 xl:grid-cols-5">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Products;
